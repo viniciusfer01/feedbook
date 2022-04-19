59410aa9203e57d714b231a4fdfa80975f0283fa
Elisa Gabriela <elisa.gabriela.lucena@gmail.com>
2022-04-19 18:16:53 -0300

    listagem estatica dos feedbacks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
+const FEEDBACK = ['bom trabalho', 'hoje você trabalhou muito bem!', 'obrigado por não ter comido meu bolinho (hoje)']
+
 function App() {
   return (
     <div className="App">
       <h1>Feedbook!</h1>
+      {FEEDBACK.map((feedback) => {
+        return <p>{feedback}</p>
+      })}
     </div>
   );
 }
