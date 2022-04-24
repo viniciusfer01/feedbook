f8d8dba248b365c88c77b4625e8e713e28ddfd93
Gabriela Targino <targinogabriela@gmail.com>
2022-04-24 15:59:45 -0300

    adição do botão para envio de feedbacks

--- src/pages/PrivateFeedbackList.js
+++ src/pages/PrivateFeedbackList.js
@@ -1,7 +1,27 @@
 import Header from "../components/Header";
+import { useState } from "react";
 
 const PrivateFeedbackList = (props) => {
   const feedbacks = props.feedbacks;
+  const [isInputActive, setisInputActive] = useState(false);
+
+  function botaoEnviarFeedback(){
+    if(!isInputActive){
+      return (
+        <div className="botao">
+        <button onClick={setisInputActive(true)}>Enviar Feedback</button>;
+      </div>
+      )
+    }
+
+    else{
+      return (<>
+      <input type='text' placeholder='Escreva seu feedback' />
+      <button>Enviar</button>
+      </>
+      )
+    }
+  }
 
   return (
     <>
@@ -11,6 +31,8 @@ const PrivateFeedbackList = (props) => {
           return <p className="feedback">{feedback}</p>;
         })}
       </div>
+
+      {botaoEnviarFeedback()}
     </>
   );
 };
